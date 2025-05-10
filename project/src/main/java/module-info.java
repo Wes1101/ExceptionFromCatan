8195fbb73edd861f71addcbfb42c4a188bf9fabe
David Willig <willig.david-it24@it.dhbw-ravensburg.de>
2025-05-10 15:24:53 +0200

    Delete basis Programm
    Signed-off-by: David Willig <willig.david-it24@it.dhbw-ravensburg.de>

--- project/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.exeptionfromcatan {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.exeptionfromcatan to javafx.fxml;
-    exports org.example.exeptionfromcatan;
-}
