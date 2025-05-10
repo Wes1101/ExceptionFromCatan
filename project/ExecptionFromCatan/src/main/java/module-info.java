26b725f04e253ecff1de7c7defe4f2e9b012e096
David Willig <willig.david-it24@it.dhbw-ravensburg.de>
2025-05-10 15:32:06 +0200

    Second try to setup javaFX project with maven
    Signed-off-by: David Willig <willig.david-it24@it.dhbw-ravensburg.de>

---
+++ project/ExecptionFromCatan/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.execptionfromcatan {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.execptionfromcatan to javafx.fxml;
+    exports org.example.execptionfromcatan;
+}
