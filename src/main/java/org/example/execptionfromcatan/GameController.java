86fc131c52cd0d68de112cea95eab9d678e47175
f-wllr <80111267+f-wllr@users.noreply.github.com>
2025-05-21 22:15:48 +0200

    add GameController class

---
+++ src/main/java/org/example/execptionfromcatan/GameController.java
@@ -0,0 +1,17 @@
+package org.example.execptionfromcatan;
+
+public class GameController {
+    public GameController() {
+
+    }
+
+    public void gameStart() {}
+
+    public void mainGame() {}
+
+    public void gameEnd() {}
+
+    public int rollDice() {
+        return 0;
+    }
+}
