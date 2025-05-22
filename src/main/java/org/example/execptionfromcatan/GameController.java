668ecd85c93e2531f080ef98c6d88ea941bb16db
f-wllr <80111267+f-wllr@users.noreply.github.com>
2025-05-22 08:26:39 +0200

    add constructor and docstring

--- src/main/java/org/example/execptionfromcatan/GameController.java
+++ src/main/java/org/example/execptionfromcatan/GameController.java
@@ -1,8 +1,29 @@
+/**
+ * @Author: Fabian Weller
+ * @Date: 22.05.2025
+ * @Version: 0.1
+ * GameController Class
+ * Handles all logic/interaction between objects for playing the game.
+ */
 package org.example.execptionfromcatan;
 
 public class GameController {
-    public GameController() {
 
+    private Player[] players;
+    private Player[] orderedPlayers;
+    private Bank bank;
+    private CatanBoard catanBoard;
+    private int gameRound;
+    private int dice1;
+    private int dice2;
+    private int vicoryPoints;
+
+    public GameController(int playerAmount, int victoryPoints) {
+        this.players = new Player[playerAmount];
+        this.bank = new Bank();
+        this.catanBoard = new CatanBoard();
+        this.gameRound = 0;
+        this.vicoryPoints = victoryPoints;
     }
 
     public void gameStart() {}
