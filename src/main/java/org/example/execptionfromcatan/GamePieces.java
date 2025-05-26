4cabe74717239e0ff94f1370e6a73177a479aebb
atsmh <atussa.mrw@yahoo.com>
2025-05-26 11:38:56 +0200

    first code class GamePieces

--- src/main/java/org/example/execptionfromcatan/GamePieces.java
+++ src/main/java/org/example/execptionfromcatan/GamePieces.java
@@ -1,4 +1,33 @@
 package org.example.execptionfromcatan;
 
-public class GamePieces {
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * This Class is administrating all the game pieces.
+ *
+ * @Atussa Mehrawari
+ * @Version??
+ *
+ */
+
+
+@Getter
+@Setter
+public abstract class GamePieces {
+    protected HexTile location;
+
+    public GamePieces(HexTile location) {
+        this.location = location;
+    }
+
+    public void setLocation(HexTile location) {
+       this.location = location;
+    }
+
+    public abstract String getType();
 }
+
+
+
+
