2f8d5bf3402a30a32ad42ef1814f45f5f587939c
David Willig <willig.david-it24@it.dhbw-ravensburg.de>
2025-05-26 19:44:29 +0200

    Renaming HelloController to ClientController

--- src/main/java/de/dhbw/app/ClientController.java
+++ src/main/java/de/dhbw/app/ClientController.java
@@ -3,7 +3,7 @@ package de.dhbw.app;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ClientController {
   @FXML
   private Label welcomeText;
 
