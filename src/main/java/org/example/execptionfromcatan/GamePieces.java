a9dda15d9f20892e63414a7c40fccc9995d97c89
atsmh <atussa.mrw@yahoo.com>
2025-05-27 19:18:49 +0200

    first code class GamePieces

--- src/main/java/org/example/execptionfromcatan/GamePieces.java
+++ src/main/java/org/example/execptionfromcatan/GamePieces.java
@@ -2,6 +2,7 @@ package org.example.execptionfromcatan;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.example.execptionfromcatan.tiles.HexTile;
 
 /**
  * This Class is administrating all the game pieces.
