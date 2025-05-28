e419fff159b12c87505fe2c2b3fa8cf98dcb5509
simon <lieblsimon0304@gmail.com>
2025-05-28 08:51:12 +0200

    Update for Menue Window and POM.xml Update of main.class

--- src/main/java/org/example/execptionfromcatan/Menue.java
+++ src/main/java/org/example/execptionfromcatan/Menue.java
@@ -13,20 +13,22 @@ public class Menue extends Application {
     @Override
     public void start(Stage menue) {
         /*Stage Settings*/
-        menue.setTitle("Exception from Catan: SiedlungsOverflow");
-        menue.setWidth(1152);
-        menue.setHeight(648);
-        menue.setResizable(false);
+        menue.setTitle("Exception from Catan: SiedlungsOverflow"); // Setzt den Titel des Fensters
+        menue.setWidth(1152);// Setzt die Breite des Fensters
+        menue.setHeight(648);// Setzt die Höhe des Fensters
+        menue.setResizable(false);// Verhindert, dass das Fenster in der Größe verändert werden kann
+/*Product until here is a empty Window 1152x648px with the name "Exception from ..." */
 
         /*Pane Settings*/
         VBox ground = new VBox();
         menue.setScene(new Scene(ground, Color.WHITE));
+/*Product got a VBox which is used to set the Background to white and serve as the first scene*/
 
-        // SplitPane erstellen
+        // SplitPane first erstellen
         SplitPane firstsplit = new SplitPane();
         firstsplit.setPrefSize(1152, 648);//Setzt die Größe des SplitPanes
         firstsplit.setDividerPositions(0.333); // Setzt die Position des Teilers auf 33%
-
+/*Product is split by a splitpane which splits the menue into a left part 2/6 big and a right part 4/6big*/
         // Linkes Pane (pink)
         StackPane leftPane = new StackPane();// Erstellen eines StackPane für das linke Pane
         leftPane.setStyle("-fx-background-color: pink;");// Setzt die Hintergrundfarbe des linken Panes auf Pink
@@ -36,12 +38,15 @@ public class Menue extends Application {
         StackPane rightPane = new StackPane();// Erstellen eines StackPane für das rechte Pane
         rightPane.setStyle("-fx-background-color: lightblue;");
         rightPane.setPrefSize(576, 324);// Setzt die Größe des rechten Panes
+/*Two Stack Panes are used to change the background colour of the splitpanes for better understanding which panes which*/
 
         // Beide Panes zum SplitPane hinzufügen
         firstsplit.getItems().addAll(leftPane, rightPane);// Fügt die beiden StackPanes zum SplitPane hinzu
 
+
         // SplitPane zum Hauptlayout hinzufügen
         ground.getChildren().add(firstsplit);// Fügt das SplitPane zum Hauptlayout (VBox) hinzu
+/*MAJOR Window 1152x648 split into 2 separate parts */
 
         SplitPane firstverticalSplit = new SplitPane();
         firstverticalSplit.setOrientation(javafx.geometry.Orientation.VERTICAL);
