2c1e1e35680d017c88b1d30df6cee0cf9ee23d21
Johann Floegel <j.floegel@aol.com>
2025-06-13 11:01:25 +0200

    update storage

--- src/main/java/de/dhbw/player/Storage.java
+++ src/main/java/de/dhbw/player/Storage.java
@@ -1,5 +1,6 @@
 package de.dhbw.player;
 
+import de.dhbw.gamePieces.BuildingTypes;
 import de.dhbw.gamePieces.City;
 import de.dhbw.gamePieces.Settlement;
 import de.dhbw.gamePieces.Street;
@@ -15,29 +16,43 @@ public class Storage {
     List<City> cities;
     List<Street> streets;
     List<Street> cards;
-    EnumMap<Resources, Integer> resources;
+    EnumMap<Resources, Integer> resourceStorage;
 
-    public Storage() {
+    public Storage(Player player, EnumMap<Resources, Integer> resources, EnumMap<BuildingTypes, Integer> buildings) {
         settlements = new ArrayList<>();
         cities = new ArrayList<>();
         streets = new ArrayList<>();
         cards = new ArrayList<>();
-        resources = new EnumMap<>(Resources.class);
+        resourceStorage = new EnumMap<>(Resources.class);
+
+        initResources(resources);
+        initBuildings(player, buildings);
     }
 
 
-    public void fillStorage(int amountSettlements, int amountCities, int amountStreets) {
+    private void initBuildings(Player player, EnumMap<BuildingTypes, Integer> buildings) {
 
+            for (int i = 0; i < buildings.get(BuildingTypes.SETTLEMENT); i++) {
+                this.settlements.add(new Settlement(player));
+            }
+            for (int i = 0; i < buildings.get(BuildingTypes.CITY); i++) {
+                this.settlements.add(new City(player));
+            }
+            for (int i = 0; i < buildings.get(BuildingTypes.STREET); i++) {
+                this.settlements.add(new Street(player));
+            }
+    }
 
-            for (int i = 0; i < amountSettlements; i++) {
-                this.settlements.add(new Settlement(player));
-            }
-            for (int i = 0; i < amountSettlements; i++) {
-                this.settlements.add(new Settlement(player));
-            }
-            for (int i = 0; i < amountSettlements; i++) {
-                this.settlements.add(new Settlement(player));
-            }
+    private void initResources(EnumMap<Resources, Integer> resources) {
+
+        for (int i = 0; i < resources.get(BuildingTypes.SETTLEMENT); i++) {
+            this.settlements.add(new Settlement(player));
+        }
+        for (int i = 0; i < resources.get(BuildingTypes.CITY); i++) {
+            this.settlements.add(new Settlement(player));
+        }
+        for (int i = 0; i < resources.get(BuildingTypes.STREET); i++) {
+            this.settlements.add(new Settlement(player));
         }
     }
 }
