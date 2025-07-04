3182e5610599c2e79c26831d7212a2654fb9daa4
David Willig <willig.david-it24@it.dhbw-ravensburg.de>
2025-07-04 15:30:35 +0200

    Update module exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,20 +5,20 @@ module de.dhbw {
   requires static lombok;
   requires java.desktop;
   requires com.google.gson;
-    requires java.smartcardio;
+  requires java.smartcardio;
 
-    opens de.dhbw.app to javafx.fxml;
-  exports de.dhbw.app ;
+  opens de.dhbw.app to javafx.fxml;
+  exports de.dhbw.app;
 
   opens de.dhbw.frontEnd.board to javafx.fxml;
-  exports de.dhbw.frontEnd.board ;
+  exports de.dhbw.frontEnd.board;
 
-  opens de.dhbw.frontEnd.menu to javafx.fxml;
-
-  exports de.dhbw.gameController ;
+  exports de.dhbw.gameController;
   exports de.dhbw.client;
   exports de.dhbw.server;
   exports de.dhbw.gamePieces;
   exports de.dhbw.dto;
   exports de.dhbw.network;
+  exports de.dhbw.mapping;
+  exports de.dhbw.player;
 }
