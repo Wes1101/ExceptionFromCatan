c22c6fd1130972bf1cd3ac937c7ba0f12b6ec6ce
Adrian Becker <adrian.julian.becker@gmail.com>
2025-07-09 18:29:39 +0200

    street interaction

--- src/main/java/de/dhbw/frontEnd/board/SceneBoardController.java
+++ src/main/java/de/dhbw/frontEnd/board/SceneBoardController.java
@@ -10,6 +10,11 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundSize;
 
+
+import javafx.scene.shape.Rectangle;  //+++
+import javafx.scene.Node;   //+++
+
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -156,7 +161,11 @@ public class SceneBoardController implements Initializable, GameUI {
             .filter(n -> n instanceof Button)
             .forEach(n -> n.toFront());
 
-
+    tile_layer.getChildren().stream()
+            .filter(n -> n instanceof Rectangle && n.getId() != null && n.getId().startsWith("road_"))
+            .forEach(n -> n.setOnMouseClicked(evt ->
+                    System.out.println("Straße geklickt: fx:id=" + n.getId())
+            ));
   }
 
   @FXML
@@ -219,6 +228,8 @@ public class SceneBoardController implements Initializable, GameUI {
     fadeOut.setOnFinished(e -> setting_menue.setVisible(false));
     fadeOut.play();
   }
+
+
 
   public void initBoard() {
     Map<IntTupel, Tile> hexes = catanBoard.getHexTiles();
